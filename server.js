c236a3bdc348715e94fc63dd75d9b839d462b11a
BlessingAgbor <helenblessing14200@gmail.com>
2022-03-16 07:47:37 +0100

    added a welcome message2 to my database

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app= express()
 const allRoute= require('./router/router')
 app.use(express.json())
 app.use('/api/v1', allRoute )
-app.get("/", (req, res)=> {
-    res.end("welcome to my database where i have stored the data of 50 Schools with images alongside, each having a distinct ID",
-    "From here you can now navigate through the different routes on my DB  ",
-    "To get all school data this in the route: '/api/v1/get'"
-    )
-})
+// app.get("/", (req, res)=> {
+//     res.end("welcome to my database where i have stored the data of 50 Schools with images alongside, each having a distinct ID",
+//     "From here you can now navigate through the different routes on my DB  ",
+//     "To get all school data this in the route: '/api/v1/get'"
+//     )
+// })
 
 
 app.listen(port, () => {
